019b339b20d50c21e439bcdc35ba7530567a8307
AlleksDev <aimr2910@gmail.com>
2026-04-29 20:50:42 -0600

    cv button

--- src/components/organisms/AboutSection/AboutSection.jsx
+++ src/components/organisms/AboutSection/AboutSection.jsx
@@ -15,7 +15,12 @@ const AboutSection = () => {
           <p>Soy <strong>Estudiante de Ingeniería en Software</strong> y Desarrollador Full Stack con sólidas bases en diseño UX/UI e ilustración.</p>
           <p>Cuento con experiencia <strong>liderando y colaborando</strong> en proyectos académicos y hackathones dentro de equipos multidisciplinarios.</p>
           <p>Mi enfoque combina la <strong>resolución técnica de problemas</strong> con la creación de interfaces intuitivas y estéticas, buscando siempre el balance perfecto entre código limpio y una experiencia de usuario excepcional.</p>
-          <button onClick={() => document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' })} className="about-btn"><div className="dot"></div> Open to work</button>
+          <div className='about-buttons'>
+            <button onClick={() => document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' })} className="about-btn"><div className="dot"></div> Open to work</button>
+            <button onClick={() => document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' })} className="cv-btn">
+              ¿Quieres ver mi CV?
+            </button>
+          </div>
         </div>
         <div className="about-image">
           <StackedCardImage src={profilePic} alt="Foto de perfil" />
