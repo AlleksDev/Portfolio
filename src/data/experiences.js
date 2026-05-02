c6647d0f136b6a14b4f0b6c7f0c520f2da3b8cbf
AlleksDev <aimr2910@gmail.com>
2026-05-01 21:05:22 -0600

    Experience Section

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,29 +1,32 @@
 const experiences = [
-  {
-    id: 1,
-    company: 'VIS DE ESTRUCTURAS',
-    place: 1,
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.',
-    tags: ['React', 'Node.js', 'MongoDB'],
-  },
-  {
-    id: 2,
-    company: 'HACKS DE REALIDAD',
-    place: 2,
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.',
-    tags: ['Python', 'AR', 'WebGL'],
-  },
-  {
-    id: 3,
-    company: 'HACKATHON',
-    place: 3,
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.',
-    tags: ['TypeScript', 'Next.js', 'Tailwind'],
-  },
-];
+    {
+      num: '01',
+      title: 'TECH SIN FRONTERAS',
+      company: 'Vara Network',
+      date: 'Nov. 2023',
+      desc: 'Participación como Front-End Developer y UX/UI Designer. El equipo tuvo un desempeño destacado logrando rapidez de ejecución y excelencia técnica bajo presión, desarrollando una plataforma orientada al entretenimiento y alivio de estrés.',
+      tags: ['Design', 'UX/UI', 'Front-End'],
+      award: '3er lugar',
+    },
+    {
+      num: '02',
+      title: 'VARA SIDE HACKATHON',
+      company: 'Vara Network',
+      date: 'Oct. 2024',
+      desc: 'Desarrollo de VIXEL, una plataforma Web3 descentralizada orientada a la creación de torneos y streaming. Participación clave en branding, conceptualización UI/UX y creación de assets visuales y funcionales del juego.',
+      tags: ['Web3', 'UX/UI', 'Game Assets'],
+      award: '1er lugar',
+    },
+    {
+      num: '03',
+      title: 'VARA MEGAHACKATHON',
+      company: 'Vara Network',
+      date: 'Ene. 2025',
+      desc: 'Competencia internacional en la que destacamos en la categoría Avanzada. Continuación del desarrollo de VIXEL demostrando innovación y éxito competitivo desarrollando aplicaciones en la red de Vara.',
+      tags: ['Web3', 'Front-End'],
+      award: '2do lugar',
+    }
+  ];
 
 export default experiences;
 
